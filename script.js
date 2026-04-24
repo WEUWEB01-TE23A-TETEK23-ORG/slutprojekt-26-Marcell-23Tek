19b2f3d243498612de5b1c411e449c60d93eca47
Marcell-23Tek <marcell.mihok@elev.ga.ntig.se>
2026-04-24 08:58:36 +0200

    jobbade på mitt slutprojekt

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
-function explore() {
-    document.getElementById("hud").innerHTML = "You wander through the forest. Suddenly you find a bear in front of you. What do you do?";
+
+var i = 0;
+
+var situationHud = document.getElementById("hud");
+
+var opt1 = document.getElementById("btn1");
+var opt2 = document.getElementById("btn2");
+var opt3 = document.getElementById("btn3");
+
+var itemHud = document.getElementById("items");
+var itemBar = document.getElementById("itemBar");
+
+function option1() {
+    if(i === 0 || i === 1){
+        main("bear")
+        i += 1;
+    }
+
+}
+function option2() {
+    if(i === 0){
+        main("bottle")
+        i += 1;
+    }
+
+}
+function option3() {
+    
+}
+function main(situation) {
+    if (situation == "bear"){
+        situationHud.textContent = "You encountered a grizzly bear. What do you do?"
+
+        opt1.textContent = "Run away";
+        opt2.textContent = "Try to fight it";
+        opt3.textContent = "Tame it";
+    }
+    if (situation == "run"){
+        // Change to death screen "Muled by bear"
+        // Add pitures for every scenario
+    }
+
+    if (situation == "bottle"){
+        situationHud.textContent = "You found a Bottle of cheap Russian Vodka. How do you continue your journey?";
+        
+        opt2.textContent = "Drink the Vodka Bottle"
+
+        itemBar.textContent += "Vodka"
+
+        itemHud.style.display = "block";
+    }
 }
