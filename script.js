19f596941605537a059569c407316c3a1276391c
Marcell-23Tek <marcell.mihok@elev.ga.ntig.se>
2026-05-06 11:30:13 +0200

    har gjort fler situationer och utvecklat möjligheterna, gjort en till sida som visas när man dör. Har utvecklat val systemet

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var i = 0;
+
 
 var situationHud = document.getElementById("hud");
 
@@ -11,21 +11,39 @@ var itemHud = document.getElementById("items");
 var itemBar = document.getElementById("itemBar");
 
 function option1() {
-    if(i === 0 || i === 1){
+    if(opt1.textContent == "Explore"){
         main("bear")
-        i += 1;
+        
+    }else if(opt1.textContent == "Run away") {
+        main("dead_bear")
     }
 
 }
 function option2() {
-    if(i === 0){
+    if(opt2.textContent == "Look around"){
         main("bottle")
-        i += 1;
-    }
+
+    }else if (opt2.textContent == "eonfeon") {
+        main("bear")
+
+    } else if (opt2.textContent == "Try again") {
+        main("restart")
+        window.location.href = "death.html";
+
+    } 
 
 }
 function option3() {
-    
+    if(opt3.textContent == "Tame it"){
+        if (itemBar.textContent.includes("Vodka")) {
+            main("Tame_bear")
+        }else{ main("dead_bear")
+        }
+       
+
+    }else if (opt3.textContent == "Stand still and wait for something to happen") {
+        main("nothing")
+    }
 }
 function main(situation) {
     if (situation == "bear"){
@@ -35,9 +53,24 @@ function main(situation) {
         opt2.textContent = "Try to fight it";
         opt3.textContent = "Tame it";
     }
-    if (situation == "run"){
+    if (situation == "dead_bear"){
+        situationHud.textContent = "You died, got mauled by a bear."
+
+        opt1.textContent = "XXXXXXX";
+        opt2.textContent = "Try again";
+        opt3.textContent = "XXXXXXX";
         // Change to death screen "Muled by bear"
         // Add pitures for every scenario
+    }
+    if (situation == "restart") {
+        situationHud.innerHTML = "You find yourself in a Forest. You have a headache and no recollection of your memories... <br> What do you do?";
+
+        opt1.textContent = "Explore";
+        opt2.textContent = "Look around";
+        opt3.textContent = "Stand still and wait for something to happen";
+
+        itemBar.textContent = ""
+        itemHud.style.display = "none";
     }
 
     if (situation == "bottle"){
@@ -45,8 +78,19 @@ function main(situation) {
         
         opt2.textContent = "Drink the Vodka Bottle"
 
-        itemBar.textContent += "Vodka"
-
+        itemBar.innerHTML += "Vodka <br>"
         itemHud.style.display = "block";
     }
+    if (situation == "Tame_bear") {
+        situationHud.innerHTML = "The bear finds the Vodka adequate, You have aquired a new companion...<br> What do you do now?"
+
+        opt1.textContent = "Explore";
+        opt2.textContent = "Look around";
+        opt3.textContent = "Stand still and wait for something to happen";
+
+        itemBar.innerHTML += "Bear Companion <br>"
+        itemHud.style.display = "block";
+    }
+
+    
 }
